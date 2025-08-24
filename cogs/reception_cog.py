ca1109d4e41966a60ee50ab08033c7c12f9dea38
fysht <f.yshx.117@gmail.com>
2025-08-24 20:40:52 +0900

    YouTubeサマリー機能を追加、ロー カル対応ver5

--- cogs/reception_cog.py
+++ cogs/reception_cog.py
@@ -5,7 +5,7 @@ import re
 import logging
 
 # --- 定数定義 ---
-YOUTUBE_URL_REGEX = re.compile(r'https?://(?:www\.)?(?:youtube\.com/watch\?v=|youtu\.be/|youtube\.com/embed/)([a-zA-Z0--9_-]{11})')
+YOUTUBE_URL_REGEX = re.compile(r'https?://(?:www\.)?(?:youtube\.com/watch\?v=|youtu\.be/|youtube\.com/embed/)([a-zA-Z0-9_-]{11})')
 
 class ReceptionCog(commands.Cog):
     """YouTubeのURL投稿を監視し、処理待ちのリアクションを付ける受付係Cog"""
