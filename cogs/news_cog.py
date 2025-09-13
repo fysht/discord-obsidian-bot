548f28cc33712932567453312a98432a39f4d958
fysht <f.yshx.117@gmail.com>
2025-09-13 18:26:47 +0900

    ニュース通知機能追加ver9

--- cogs/news_cog.py
+++ cogs/news_cog.py
@@ -18,7 +18,7 @@ from web_parser import parse_url_with_readability
 
 # --- 定数定義 ---
 JST = zoneinfo.ZoneInfo("Asia/Tokyo")
-NEWS_BRIEFING_TIME = time(hour=18, minute=15, tzinfo=JST)
+NEWS_BRIEFING_TIME = time(hour=18, minute=30, tzinfo=JST)
 
 class NewsCog(commands.Cog):
     """天気予報と株式関連ニュースを定時通知するCog"""
